b3a49a7ef81bf412750a5c563e5fa034ef0adaa0
Octave Wagner <octave.wagner@gmail.com>
2025-06-03 15:15:59 +0000

    update union_all

---
+++ models/intermediate/union_all.sql
@@ -0,0 +1,15 @@
+{{ cast_all('div', 'premier_league') }}
+UNION ALL
+({{ cast_all('div', 'liga') }})
+UNION ALL
+{{ cast_all('div', 'bundesliga') }}
+UNION ALL
+{{ cast_all('div', 'serie_a') }}
+UNION ALL
+{{ cast_all('div', 'serie_b') }}
+UNION ALL
+{{ cast_all('div', 'ligue_1') }}
+UNION ALL
+{{ cast_all('div', 'eredivisie') }}
+UNION ALL
+({{ cast_all('div', 'primeira_liga') }})
