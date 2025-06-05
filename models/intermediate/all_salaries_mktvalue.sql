b89ca63f87a6612ede16bd26bb958d77a3e7e973
Octave Wagner <octave.wagner@gmail.com>
2025-06-05 14:42:59 +0000

    rajout dossier mart : foot_table_final + modification names all_salaries_mkvalue

--- models/intermediate/all_salaries_mktvalue.sql
+++ models/intermediate/all_salaries_mktvalue.sql
@@ -1,18 +1,21 @@
 {{ config(materialized='table') }}
-WITH join_sub AS (SELECT 
-    W.player
-    , M.player_name
-    , ROUND(W.monthly_wages_eur,0) as monthly_wages_eur
-    , W.annual_wages
-    , ROUND(M.market_value_eur,0) as market_value_eur
-    , W.club
-FROM {{ ref('salaires_all') }} as W
-FULL OUTER JOIN {{ ref('players_values_eu') }} as M
-ON W.player = M.player_name)
 
-, nonull_sub AS (SELECT *
-FROM join_sub
-WHERE NOT (player IS NULL AND market_value_eur IS NULL)
+WITH join_sub AS (
+    SELECT 
+        COALESCE(W.player, M.player_name) AS Player,
+        ROUND(W.monthly_wages_eur, 0) AS Monthly_wages,
+        W.annual_wages AS Annual_Wages,
+        ROUND(M.market_value_eur, 0) AS Market_value_eur,
+        W.club AS Team
+    FROM {{ ref('salaires_all') }} AS W
+    FULL OUTER JOIN {{ ref('players_values_eu') }} AS M
+    ON W.player = M.player_name
+),
+
+nonull_sub AS (
+    SELECT *
+    FROM join_sub
+    WHERE NOT (player IS NULL AND market_value_eur IS NULL)
 )
 
 SELECT DISTINCT *
