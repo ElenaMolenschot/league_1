37c849dc8540829975fe2c5bca21ff6644ef7fb2
taylor Lozandier <taylor.lozandier@gmail.com>
2025-06-05 10:42:32 +0000

    update scoring

--- macros/compute_score_brut_standardise.sql
+++ macros/compute_score_brut_standardise.sql
@@ -21,5 +21,49 @@
       0.05 * xG_Expected_per90_ratio -
       0.05 * CrdY_per90_ratio -
       0.05 * CrdR_per90_ratio
+
+    WHEN {{ Poste_simplifie }} = 'Milieu Offensif' THEN
+    0.20 * Ast_per90_ratio +
+    0.15 * xAG_Expected_per90_ratio +
+    0.15 * SCA_SCA_per90_ratio +
+    0.10 * GCA_SCA_per90_ratio +
+    0.10 * PrgP_Passes_per90_ratio +
+    0.10 * Succ_Take_Ons_per90_ratio +
+    0.05 * xG_Expected_per90_ratio -
+    0.05 * CrdY_per90_ratio -
+    0.05 * CrdR_per90_ratio
+
+    WHEN {{ Poste_simplifie }} = 'Milieu Relayeur' THEN
+    0.15 * Cmp_Passes_per90_ratio +
+    0.15 * PrgP_Passes_per90_ratio +
+    0.10 * Carries_Carries_per90_ratio +
+    0.10 * Succ_Take_Ons_per90_ratio +
+    0.10 * Int_per90_ratio +
+    0.10 * Tkl_per90_ratio +
+    0.10 * xAG_Expected_per90_ratio +
+    0.05 * GCA_SCA_per90_ratio -
+    0.05 * CrdY_per90_ratio -
+    0.05 * CrdR_per90_ratio
+
+    WHEN {{ Poste_simplifie }} = 'Milieu Relayeur' THEN
+    0.20 * Tkl_per90_ratio +
+    0.20 * Int_per90_ratio +
+    0.15 * Blocks_per90_ratio +
+    0.10 * Cmp_Passes_per90_ratio +
+    0.10 * PrgP_Passes_per90_ratio +
+    0.10 * Touches_per90_ratio -
+    0.10 * CrdY_per90_ratio -
+    0.05 * CrdR_per90_ratio
+
+    WHEN  {{ Poste_simplifie }} = 'Milieu Défensif' THEN
+    0.20 * Tkl_per90_ratio +
+    0.20 * Int_per90_ratio +
+    0.15 * Blocks_per90_ratio +
+    0.10 * Cmp_Passes_per90_ratio +
+    0.10 * PrgP_Passes_per90_ratio +
+    0.10 * Touches_per90_ratio -
+    0.10 * CrdY_per90_ratio -
+    0.05 * CrdR_per90_ratio
+
   END
 {% endmacro %}
