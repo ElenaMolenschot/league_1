9b3ea0c0b4d40d4dc9b0d6f1315db4b54103966d
Octave Wagner <octave.wagner@gmail.com>
2025-06-09 14:35:10 +0000

    update jointure de foot_table_final

--- models/mart/foot_table_final.sql
+++ models/mart/foot_table_final.sql
@@ -1,83 +1,84 @@
-{{ config(materialized='table') }}
-
 WITH int_top AS (
   SELECT *,
          LOWER(
-           TRANSLATE(Player,
+           TRANSLATE(
+             ARRAY_TO_STRING(
+               ARRAY(
+                 SELECT word
+                 FROM UNNEST(SPLIT(Player, ' ')) AS word
+                 ORDER BY word
+               ), ' '
+             ),
              "ÁÀÂÄÃÅÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝÑÇáàâäãåéèêëíìîïóòôöõúùûüýñç",
              "AAAAAAEEEEIIIIOOOOOUUUUYNCaaaaaaeeeeiiiiooooouuuuync"
            )
-         ) AS player_clean
+         ) AS sorted_player_key
   FROM {{ ref('int_top_players') }}
 ),
 
-players_cleaned AS (
+players_dedup AS (
   SELECT *,
          LOWER(
-           TRANSLATE(Player,
+           TRANSLATE(
+             ARRAY_TO_STRING(
+               ARRAY(
+                 SELECT word
+                 FROM UNNEST(SPLIT(Player, ' ')) AS word
+                 ORDER BY word
+               ), ' '
+             ),
              "ÁÀÂÄÃÅÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝÑÇáàâäãåéèêëíìîïóòôöõúùûüýñç",
              "AAAAAAEEEEIIIIOOOOOUUUUYNCaaaaaaeeeeiiiiooooouuuuync"
            )
-         ) AS player_clean
+         ) AS sorted_player_key,
+         ROW_NUMBER() OVER (
+           PARTITION BY LOWER(
+             TRANSLATE(
+               ARRAY_TO_STRING(
+                 ARRAY(
+                   SELECT word
+                   FROM UNNEST(SPLIT(Player, ' ')) AS word
+                   ORDER BY word
+                 ), ' '
+               ),
+               "ÁÀÂÄÃÅÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝÑÇáàâäãåéèêëíìîïóòôöõúùûüýñç",
+               "AAAAAAEEEEIIIIOOOOOUUUUYNCaaaaaaeeeeiiiiooooouuuuync"
+             )
+           )
+           ORDER BY Market_value_eur DESC
+         ) AS rn
   FROM {{ ref('players_values_eu') }}
 ),
+players_dedup_filtered AS (
+  SELECT *
+  FROM players_dedup
+  WHERE rn = 1
+),
 
--- On génère toutes les paires joueur court vs joueur long
-cross_match AS (
-  SELECT
-    S.Player,
+joined_players AS (
+  SELECT 
     S.League,
+    S.Player,
     S.Team,
     S.Age,
     S.Nombre_Matchs,
     S.Poste_simplifie,
     S.score_99,
-    W.Player AS Player_ref,
     W.Market_value_eur,
     W.Team AS Team_salaries,
-    W.player_clean AS player_clean_ref,
-    SPLIT(S.player_clean, ' ') AS player_words
+    ARRAY_LENGTH(
+      ARRAY(
+        SELECT word
+        FROM UNNEST(SPLIT(S.sorted_player_key, ' ')) AS word
+        WHERE word IN UNNEST(SPLIT(W.sorted_player_key, ' '))
+      )
+    ) AS common_words,
+    ARRAY_LENGTH(SPLIT(S.sorted_player_key, ' ')) AS total_words
   FROM int_top AS S
-  CROSS JOIN players_cleaned AS W
-),
-
--- On vérifie que TOUS les mots du joueur court apparaissent dans le nom du joueur long
-filtered_matches AS (
-  SELECT *,
-    (
-      SELECT COUNT(*) 
-      FROM UNNEST(player_words) AS word
-      WHERE player_clean_ref LIKE CONCAT('%', word, '%')
-    ) AS match_count,
-    ARRAY_LENGTH(player_words) AS total_words
-  FROM cross_match
-),
-
--- On garde les correspondances complètes
-valid_matches AS (
-  SELECT *,
-         ROW_NUMBER() OVER (PARTITION BY Player ORDER BY Market_value_eur DESC) AS rn
-  FROM filtered_matches
-  WHERE match_count = total_words
-),
-
-players_dedup AS (
-  SELECT *
-  FROM valid_matches
-  WHERE rn = 1
+  LEFT JOIN players_dedup_filtered AS W ON TRUE
 )
 
-SELECT
-  S.League,
-  S.Player,
-  S.Team,
-  S.Age,
-  S.Nombre_Matchs,
-  S.Poste_simplifie,
-  S.score_99,
-  W.Market_value_eur,
-  W.Team_salaries
-FROM int_top AS S
-LEFT JOIN players_dedup AS W
-  ON S.Player = W.Player
-ORDER BY S.score_99 DESC
+SELECT * EXCEPT (common_words, total_words)
+FROM joined_players
+WHERE common_words = total_words
+ORDER BY score_99 DESC
--- models/mart/test.sql
+++ models/mart/test.sql
@@ -1,2 +1,84 @@
-SELECT COUNT(*) FROM {{ ref('foot_table_final') }}
-WHERE Market_value_eur IS NOT NULL
+WITH int_top AS (
+  SELECT *,
+         LOWER(
+           TRANSLATE(
+             ARRAY_TO_STRING(
+               ARRAY(
+                 SELECT word
+                 FROM UNNEST(SPLIT(Player, ' ')) AS word
+                 ORDER BY word
+               ), ' '
+             ),
+             "ÁÀÂÄÃÅÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝÑÇáàâäãåéèêëíìîïóòôöõúùûüýñç",
+             "AAAAAAEEEEIIIIOOOOOUUUUYNCaaaaaaeeeeiiiiooooouuuuync"
+           )
+         ) AS sorted_player_key
+  FROM {{ ref('int_top_players') }}
+),
+
+players_dedup AS (
+  SELECT *,
+         LOWER(
+           TRANSLATE(
+             ARRAY_TO_STRING(
+               ARRAY(
+                 SELECT word
+                 FROM UNNEST(SPLIT(Player, ' ')) AS word
+                 ORDER BY word
+               ), ' '
+             ),
+             "ÁÀÂÄÃÅÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝÑÇáàâäãåéèêëíìîïóòôöõúùûüýñç",
+             "AAAAAAEEEEIIIIOOOOOUUUUYNCaaaaaaeeeeiiiiooooouuuuync"
+           )
+         ) AS sorted_player_key,
+         ROW_NUMBER() OVER (
+           PARTITION BY LOWER(
+             TRANSLATE(
+               ARRAY_TO_STRING(
+                 ARRAY(
+                   SELECT word
+                   FROM UNNEST(SPLIT(Player, ' ')) AS word
+                   ORDER BY word
+                 ), ' '
+               ),
+               "ÁÀÂÄÃÅÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝÑÇáàâäãåéèêëíìîïóòôöõúùûüýñç",
+               "AAAAAAEEEEIIIIOOOOOUUUUYNCaaaaaaeeeeiiiiooooouuuuync"
+             )
+           )
+           ORDER BY Market_value_eur DESC
+         ) AS rn
+  FROM {{ ref('players_values_eu') }}
+),
+players_dedup_filtered AS (
+  SELECT *
+  FROM players_dedup
+  WHERE rn = 1
+),
+
+joined_players AS (
+  SELECT 
+    S.League,
+    S.Player,
+    S.Team,
+    S.Age,
+    S.Nombre_Matchs,
+    S.Poste_simplifie,
+    S.score_99,
+    W.Market_value_eur,
+    W.Team AS Team_salaries,
+    ARRAY_LENGTH(
+      ARRAY(
+        SELECT word
+        FROM UNNEST(SPLIT(S.sorted_player_key, ' ')) AS word
+        WHERE word IN UNNEST(SPLIT(W.sorted_player_key, ' '))
+      )
+    ) AS common_words,
+    ARRAY_LENGTH(SPLIT(S.sorted_player_key, ' ')) AS total_words
+  FROM int_top AS S
+  LEFT JOIN players_dedup_filtered AS W ON TRUE
+)
+
+SELECT * EXCEPT (common_words, total_words)
+FROM joined_players
+WHERE common_words = total_words
+ORDER BY score_99 DESC
