3f380fedd799940779e76a15804070c625b7a00a
taylor Lozandier <taylor.lozandier@gmail.com>
2025-06-09 09:59:35 +0000

    OCtave

--- models/mart/foot_table_final.sql
+++ models/mart/foot_table_final.sql
@@ -1,3 +1,14 @@
-SELECT * FROM {{ ref('int_top_players') }}
-INNER JOIN {{ ref('all_salaries_mktvalue') }}
+SELECT 
+Player,
+tp.Team,
+Nombre_Matchs,
+Poste_simplifie,
+score_brut,
+score_99,
+Monthly_wages,
+Annual_Wages,
+Market_value_eur
+
+FROM {{ ref('int_top_players') }} AS tp
+INNER JOIN {{ ref('all_salaries_mktvalue') }} AS mkt
 USING(Player)
